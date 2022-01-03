d33ac9542fdff60a01d26ba387c6e622f25720f1
charlek <93940108+sharkycharky@users.noreply.github.com>
2022-01-03 18:15:40 +0000

    I was able to use the format of Part 1 but it took me a bit to figure out (with the help of my classmate) how to set the value of cubes. I originally had it as cubes = i*i*i rather than cubes = cubes + i*i*i, but then it only did that for the smallest number. I also had a bit of trouble with the start/stop/step formula but fixed it somewhat quickly.

--- part2.py
+++ part2.py
@@ -9,3 +9,11 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 
 #write your code here
 
+number = int (input ("Enter a positive integer: "))
+cubes = 0
+
+for i in range (number, 0, -1):
+  cubes = cubes + i * i * i
+   
+
+print(cubes)
